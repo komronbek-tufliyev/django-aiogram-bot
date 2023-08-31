9b16a3aadfe18688904b29b7fc9d29c168dfd548
Komronbek Tufliyev <komronbek773@gmail.com>
2023-08-31 12:03:50 +0500

    added app models to admins.py and created translation.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+from .translation import CategoryTranslationOptions, ProductTranslationOptions
+from modeltranslation.admin import TranslationAdmin
+
+@admin.register(BotUser)
+class BotUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'telegram_id', 'language')
+    search_fields = ('name', 'telegram_id', 'language')
+    list_filter = ('language', 'added')
+    list_per_page = 10
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'latitude', 'longitude')
+    # search_fields = ('user', 'latitude', 'longitude')
+    # list_filter = ('user', 'latitude', 'longitude')
+    list_per_page = 10
+
+@admin.register(Category)
+class CategoryAdmin(TranslationAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+@admin.register(Product)
+class ProductAdmin(TranslationAdmin):
+    list_display = ('picture', 'name', 'category', 'price', 'discount')
+    search_fields = ('name', 'category', 'price', 'discount')
+    list_filter = ('category', 'price', 'discount')
+    list_per_page = 10
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'all_shop', 'all_products')
+    list_per_page = 10
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'shop')
+    list_per_page = 10
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from urllib.parse import urlparse
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.urls.base import resolve, reverse
+from django.urls.exceptions import Resolver404
